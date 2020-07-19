846ea404a65acc66932e93c79a806ecfd28720f7
yuriyfomin17 <fominyura2012@gmail.com>
2020-07-19 22:25:11 +0100

    improved

--- src/App.js
+++ src/App.js
@@ -5,43 +5,62 @@ function App() {
     const [const1, setConst1] = useState(5);
     const [const2, setConst2] = useState(5);
     const [const3, setConst3] = useState(5);
-    const [change, randomSetter] = useState();
-    const [word, wordSetter] = useState();
+    const [word, wordSetter] = useState(undefined);
+    const [change, randomSetter] = useState(undefined);
     const increment = () => {
-        console.log('Increment', const1);
         setConst1(const1 + 1);
-        setConst2(const2 + 10)
-        let curr_change = Math.floor(Math.random() * 11)
-        randomSetter(curr_change)
-        setConst3(const3 + curr_change)
-        wordSetter('increased')
     };
     const decrement = () => {
-        console.log('Minus One', const1);
+
         setConst1(const1 - 1);
-        setConst2(const2 - 10)
-        let curr_change = Math.floor(Math.random() * 11)
-        randomSetter(curr_change)
-        setConst3(const3 - curr_change)
-        wordSetter('decreased')
+
+
+    };
+    const Add10 = () => {
+        setConst2(const2 + 10);
+    };
+    const Subtract10 = () => {
+        setConst2(const2 - 10);
+    };
+
+    const addRandom = () => {
+        const curr_change = Math.floor(Math.random() * 11)
+        setConst3(const3 + curr_change);
+        wordSetter('was increased')
+        randomSetter('by ' + curr_change)
+    };
+    const SubtractRandom = () => {
+        const curr_change = Math.floor(Math.random() * 11)
+        setConst3(const3 - curr_change);
+        wordSetter('was decreased')
+        randomSetter('by ' + curr_change)
     };
 
     return (
         <div>
-            <button onClick={increment}>Increment</button>
             <div>
-                One counter: {const1}
-
-            </div>
-            Ten counter: {const2}
-            <div>
-            </div>
-            Random Counter: {const3} was {word} by {change}
-            <div>
-
+                <button onClick={increment}>Increment</button>
+                One counter
+                : {const1}
                 <button onClick={decrement}>Decrement</button>
             </div>
+            <br/>
+            <div>
+                <button onClick={Add10}>Add 10</button>
+                10 Counter
+                : {const2}
+                <button onClick={Subtract10}>Subtract 10</button>
+            </div>
+            <br/>
+            <div>
+                <button onClick={addRandom}>Add random number</button>
+                Random Counter
+                : {const3} {word} {change}
+                <button onClick={SubtractRandom}>Subtract random number</button>
+            </div>
+
         </div>
+
     );
 }
 
